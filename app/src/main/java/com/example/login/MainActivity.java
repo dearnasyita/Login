75d7f22131740bbccebb239c7fc9e972f716f55f
dearnas <floramode19@gmail.com>
2020-10-13 01:31:17 +0700

    done

--- app/src/main/java/com/example/login/MainActivity.java
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -1,5 +1,20 @@
 package com.example.login;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
@@ -10,11 +25,73 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView username, email, nama, alamat, asalSekolah;
+    Button btnLogout;
+    public static final String FILENAME = "login";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().setTitle("Data User");
+
+        username = findViewById(R.id.tvUsername);
+        email = findViewById(R.id.tvEmail);
+        nama = findViewById(R.id.tvNamaLengkap);
+        asalSekolah = findViewById(R.id.tvAsalSekolah);
+        alamat = findViewById(R.id.tvAlamat);
+        bacaFileLogin();
+
     }
+    void bacaFileLogin(){
+        File sdcard = getFilesDir();
+        File file = new File(sdcard, FILENAME);
+        if (file.exists()){
+            StringBuilder text = new StringBuilder();
+            try {
+                BufferedReader br = new BufferedReader(new FileReader(file));
+                String line = br.readLine();
+                while (line != null){
+                    text.append(line);
+                    line = br.readLine();
+                }
+                br.close();
+            } catch (IOException e){
+                System.out.println("Error "+ e.getMessage());
+            }
+            String data = text.toString();
+            String[] dataUser = data.split(";");
+            bacaDataUser(dataUser[0]);
+        }
+    }
+    void bacaDataUser(String fileName){
+        File sdcard = getFilesDir();
+        File file = new File(sdcard, fileName);
+        if (file.exists()){
+            StringBuilder text = new StringBuilder();
+            try {
+                BufferedReader br = new BufferedReader(new FileReader(file));
+                String line = br.readLine();
+                while (line != null){
+                    text.append(line);
+                    line = br.readLine();
+                }
+                br.close();
+            } catch (IOException e){
+                System.out.println("Error "+e.getMessage());
+            }
+            String data = text.toString();
+            String[] dataUser = data.split(";");
+            username.setText(dataUser[0]);
+            email.setText(dataUser[2]);
+            nama.setText(dataUser[3]);
+            asalSekolah.setText(dataUser[4]);
+            alamat.setText(dataUser[5]);
+        } else {
+            Toast.makeText(this, "User tidak ditemukan", Toast.LENGTH_SHORT).show();
+        }
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -25,7 +102,9 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-      if(item.getItemId() == R.id.id_logout)
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        if(item.getItemId() == R.id.id_logout)
+        startActivity(intent);
             finish();
         return true;
     }
